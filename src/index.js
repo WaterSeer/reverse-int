e6e7137a848ef09e0e8858e07b3f9ee5c97b9cb2
Mike Kargin <41961514+WaterSeer@users.noreply.github.com>
2021-03-14 06:06:36 +0300

    Update index.js
    implement function reverse()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let arr = n.toString().split('');
+  let arrRev = [];
+  for(let i = 0; i < arr.length; i++)
+      arrRev[i] = arr[arr.length - 1 - i];
+  return parseInt(arrRev.join(''), 10);
 }
